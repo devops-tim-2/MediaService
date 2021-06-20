b7b6b7eb7d35500665498703098c2232e1e896a1
nenad-misic <33166172+nenad-misic@users.noreply.github.com>
2021-06-20 23:52:36 +0200

    Add image upload functionality close #1 (#6)

---
+++ main.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, request
+
+from os import mkdir, environ
+from os.path import exists, join
+import uuid
+
+UPLOAD_FOLDER = environ.get('IMAGE_UPLOAD_PATH')
+
+app = Flask(__name__)
+
+
+if not exists(UPLOAD_FOLDER):
+    mkdir(UPLOAD_FOLDER)
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    image = request.files['image']
+    filename = f"{str(uuid.uuid4())}.{image.filename.split('.')[-1]}"
+    path = join(UPLOAD_FOLDER, filename)
+    image.save(path)
+    return { 'image_path': filename }
+
+
+if __name__ == '__main__':
+    app.run(host=environ.get('FLASK_RUN_HOST'))
